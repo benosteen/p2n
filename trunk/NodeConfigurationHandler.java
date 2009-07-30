42f8f2295552548a19a5d66d2956bd718bfc3847
davetaz <davetaz@ac69efe2-6199-11de-8c10-5fe6993ea61f>
2009-07-30 13:15:59 +0000

    * More optimised

--- trunk/NodeConfigurationHandler.java
+++ trunk/NodeConfigurationHandler.java
@@ -18,51 +18,57 @@ class NodeConfigurationHandler {
 	public NodeConfigurationHandler() {
 	}
 
+
 	public Hashtable get_configuration_from_file(String path) {
 		Hashtable settings = new Hashtable();
 		try {
-			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-			Document doc = docBuilder.parse (new File(path));
-	
-			doc.getDocumentElement ().normalize ();
-			Element root = doc.getDocumentElement();
-			for (Node node = root.getFirstChild(); node != null; node=node.getNextSibling()) {
-				try {
-					String node_name = "";
-					String node_value = "";
-					node_name = node.getNodeName();
-					if (node_name.equals("node")) {
-						Node inner_node = node.getFirstChild().getNextSibling();
-						String node_url = (((Element)inner_node).getFirstChild()).getNodeValue();
-						System.out.println("Creating node with url " + node_url);
-						PSNNode psnnode = new PSNNode(node_url);
-						settings = add_to_config(node_name,psnnode,settings);
-						continue;
-					}
-					if (node_name.equals("keypair")) {
-						Node inner_node = node.getFirstChild().getNextSibling();
-						String access_id = (((Element)inner_node).getFirstChild()).getNodeValue();
-						inner_node = inner_node.getNextSibling().getNextSibling().getFirstChild();
-						String private_key  = inner_node.getNodeValue();
-						Keypair kp = new Keypair(access_id,private_key);
-						settings = add_to_config(node_name,kp,settings);
-						continue;
-					}
-					if (node_name.equals("#text")) {
-						continue;
-					}
-					node_value = ((Element)node).getFirstChild().getNodeValue();
-					settings = add_to_config(node_name,node_value,settings);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			return settings;
+                        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+                        Document doc = docBuilder.parse (new File(path));
+
+                        doc.getDocumentElement ().normalize ();
+                        Element root = doc.getDocumentElement();
+			settings = get_configuration_from_root(root);
 		} catch (Exception e) {
 			e.printStackTrace();
-			return settings;
 		}
+		return settings;
+	}
+
+	public Hashtable get_configuration_from_root(Element root) {
+		Hashtable settings = new Hashtable();
+		for (Node node = root.getFirstChild(); node != null; node=node.getNextSibling()) {
+			try {
+				String node_name = "";
+				String node_value = "";
+				node_name = node.getNodeName();
+				if (node_name.equals("node")) {
+					Node inner_node = node.getFirstChild().getNextSibling();
+					String node_url = (((Element)inner_node).getFirstChild()).getNodeValue();
+					System.out.println("Creating node with url " + node_url);
+					PSNNode psnnode = new PSNNode(node_url);
+					settings = add_to_config(node_name,psnnode,settings);
+					continue;
+				}
+				if (node_name.equals("keypair")) {
+					Node inner_node = node.getFirstChild().getNextSibling();
+					String access_id = (((Element)inner_node).getFirstChild()).getNodeValue();
+					inner_node = inner_node.getNextSibling().getNextSibling().getFirstChild();
+					String private_key  = inner_node.getNodeValue();
+					Keypair kp = new Keypair(access_id,private_key);
+					settings = add_to_config(node_name,kp,settings);
+					continue;
+				}
+				if (node_name.equals("#text")) {
+					continue;
+				}
+				node_value = ((Element)node).getFirstChild().getNodeValue();
+				settings = add_to_config(node_name,node_value,settings);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return settings;
 	}
 	
 	private Hashtable add_to_config(String key,Object value,Hashtable settings) {
