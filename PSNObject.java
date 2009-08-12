868b5d3e94e36ae96435d639b448e3855cfb9d6f
davetaz <davetaz@ac69efe2-6199-11de-8c10-5fe6993ea61f>
2009-08-12 15:37:46 +0000

    * Missed File

---
+++ PSNObject.java
@@ -0,0 +1,81 @@
+class PSNObject {
+	
+	String requested_path = null;
+	String uuid = null;
+	boolean local_copy = false;
+	boolean psn_copy = false;
+	int psn_distribution = 0;
+	int psn_resiliance = 0;
+	String acl = null;
+	String local_path = null;
+	String md5_sum = null;
+	String mime_type = null;
+
+	public PSNObject() {
+	}
+
+	public void setUUID(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getUUID() {
+		return uuid;
+	}
+
+	public void setRequestedPath(String requested_path) {
+		this.requested_path = requested_path;
+	}
+
+	public String getRequestedPath() {
+		return requested_path;
+	}
+
+	public void setLocalCopy(boolean local_copy) {
+		this.local_copy = local_copy;
+	}
+
+	public boolean getLocalCopy() {
+		return local_copy;
+	}
+
+	public void setPSNCopy(boolean psn_copy) {
+		this.psn_copy = psn_copy;
+	}
+
+	public boolean getPSNCopy() {
+		return psn_copy;
+	}
+
+	public void setACL(String acl) {
+		this.acl = acl;
+	}
+
+	public String getACL() {
+		return acl;
+	}
+
+	public void setLocalPath(String path) {
+		this.local_path = path;
+	}
+
+	public String getLocalPath() {
+		return local_path;
+	}
+
+	public void setMD5Sum(String sum) {
+		this.md5_sum = sum;
+	}
+
+	public String getMD5Sum() {
+		return md5_sum;
+	}
+
+	public void setMimeType(String type) {
+		this.mime_type = type;
+	}
+
+	public String getMimeType() {
+		return mime_type;
+	}
+
+}
