66a9fe8caddacc42355f91e5bd0a72cb06dbb7cf
davetaz <davetaz@ac69efe2-6199-11de-8c10-5fe6993ea61f>
2009-08-14 13:53:54 +0000

    * Missed file

---
+++ PSNNodeSelector.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class PSNNodeSelector {
+	
+	DatabaseConnector_Mysql dbm;
+
+	public PSNNodeSelector(DatabaseConnector_Mysql dbm) {
+		this.dbm = dbm;
+	}
+
+	public PSNNode getNode(Vector nodes_done) {
+		Vector active_nodes = dbm.getActiveNodes();
+		for (int i=0;i<active_nodes.size();i++) {
+			boolean done_flag = false;
+			PSNNode in = (PSNNode)active_nodes.get(i);
+			String in_url = in.get_node_url();
+			for (int j=0;j<nodes_done.size();j++) {
+				PSNNode done = (PSNNode)nodes_done.get(i);
+				String done_url = done.get_node_url();
+				if (done_url.equals(in_url)) {
+					done_flag = true;
+				}
+			}
+			if (!done_flag) {
+				return in;
+			} 
+		}
+		return null;
+	}
+
+
+}
