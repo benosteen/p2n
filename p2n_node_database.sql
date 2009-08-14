5cc099e7d9a96966014c637b1fee8799f7a51e15
davetaz <davetaz@ac69efe2-6199-11de-8c10-5fe6993ea61f>
2009-08-14 13:55:55 +0000

    * Updated database schema

--- p2n_node_database.sql
+++ p2n_node_database.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.11
 --
--- Host: localhost    Database: dct05r_p2n_node1
+-- Host: localhost    Database: p2n_node
 -- ------------------------------------------------------
--- Server version	5.0.45
+-- Server version	5.0.67-1-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,30 +20,39 @@
 --
 
 DROP TABLE IF EXISTS `Users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `Users` (
   `access_id` varchar(255) default NULL,
   `private_key` varchar(255) default NULL,
   `user_type` varchar(255) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `files`
 --
 
 DROP TABLE IF EXISTS `files`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `files` (
   `mapping_uuid` varchar(255) default NULL,
   `path` blob,
   `md5_sum` varchar(255) default NULL,
   `type` varchar(255) default NULL,
-  `mime_type` varchar(255) default NULL
+  `mime_type` varchar(255) default NULL,
+  `owner` varchar(255) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `mappings`
 --
 
 DROP TABLE IF EXISTS `mappings`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `mappings` (
   `access_id` varchar(255) default NULL,
   `requested_path` varchar(255) default NULL,
@@ -55,34 +64,43 @@ CREATE TABLE `mappings` (
   `acl` varchar(255) default NULL,
   PRIMARY KEY  (`uuid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `node_associations`
 --
 
 DROP TABLE IF EXISTS `node_associations`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `node_associations` (
   `node_id` varchar(255) default NULL,
   `access_id_owned` varchar(255) default NULL,
   `last_update` int(14) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `node_files`
 --
 
 DROP TABLE IF EXISTS `node_files`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `node_files` (
   `node_id` varchar(255) default NULL,
   `mapping_uuid` varchar(255) default NULL,
   `type` varchar(255) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `nodes`
 --
 
 DROP TABLE IF EXISTS `nodes`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `nodes` (
   `id` varchar(255) NOT NULL,
   `url` blob,
@@ -90,29 +108,36 @@ CREATE TABLE `nodes` (
   `allocated_space` int(11) default NULL,
   `last_handshake` int(14) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `object_metadata`
 --
 
 DROP TABLE IF EXISTS `object_metadata`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `object_metadata` (
   `mapping_uuid` varchar(255) default NULL,
   `word` varchar(255) default NULL,
   `value` varchar(255) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `scanning_log`
 --
 
 DROP TABLE IF EXISTS `scanning_log`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `scanning_log` (
   `mapping_uuid` varchar(255) default NULL,
   `node_id` varchar(255) default NULL,
   `message_type` varchar(255) default NULL,
   `message` blob
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -123,4 +148,4 @@ CREATE TABLE `scanning_log` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-08-12 15:33:09
+-- Dump completed on 2009-08-14 13:55:26
